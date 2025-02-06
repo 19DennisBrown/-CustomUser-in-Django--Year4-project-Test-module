11f6bb19ada6e76cda5b6fd41eeedc969d881be2
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2025-02-06 14:52:25 +0300

    static

--- servers/servers/urls.py
+++ servers/servers/urls.py
@@ -1,4 +1,10 @@
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib import admin
+from django.urls import path, include
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('userAuthe.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
